51e2357353e955208575beba679968112485b783
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-12 18:40:16 +0600

    Set all the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/students/Home'
+import CourseList from './pages/students/CoursesList'
+import CourseDetails from './pages/students/CourseDetails'
+import MyEnrollments from './pages/students/MyEnrollments'
+import Player from './pages/students/Player'
+import Loading from './components/students/Loading'
+
 import './App.css'
 
 function App() {
   return (
     <>
-     <h1 className='text-5xl text-red-500'>Hello</h1>
+     <Routes>
+      <Route path='/' element={ <Home/> } />
+      <Route path='/course-list' element={ <CourseList /> } />
+      <Route path='/course-list/:input' element={ <CourseList /> } />
+      <Route path='/course/:id' element={ <CourseDetails /> } />
+      <Route path='/my-enrollments' element={ <MyEnrollments /> } />
+      <Route path='/player/:courseId' element={ <Player /> } />
+      <Route path='/loading/:path' element={ <Loading /> } />
+     </Routes>
     </>
   )
 }
---
+++ client/src/pages/educators/Educator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Educator = () => {
+    return (
+        <div>
+            Educator
+        </div>
+    );
+};
+
+export default Educator;
