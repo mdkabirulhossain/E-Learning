e8b20ead0e2aa4fbf556c1a0a393a7810bcd9a53
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-14 00:50:37 +0600

    Navbar responsive for all device

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -10,14 +10,22 @@ const Navbar = () => {
             <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
 
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
-                <div>
+                <div className='flex items-center gap-5'>
                     <button>Become Educator</button>
                     | <Link to='/my-enrollments'>My Enrollments</Link>
                 </div>
                 <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
 
             </div>
-            <div></div>
+
+            {/* For Mobile  */}
+            <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+                <div>
+                    <button>Become Educator</button>
+                    | <Link to='/my-enrollments'>My Enrollments</Link>
+                </div>
+                <button> <img src={assets.user_icon} alt="user Icon" srcset="" /> </button>
+            </div>
         </div>
     );
 };
