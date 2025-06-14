31fb84370edb1280cf708c326832b2c085d584c8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-15 00:27:31 +0600

    Hero component done and call from Home component

--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from '../../components/students/Hero';
 
 const Home = () => {
     return (
-        <div>
-            Home Page
+        <div className='flex flex-col items-center space-y-7 text-center'>
+            {/* Home page  */}
+            <Hero />
         </div>
     );
 };
