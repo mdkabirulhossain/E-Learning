c71f41ca8e9dd0b74e517580c5d6a404c73d0321
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-18 15:15:55 +0600

    Course List component is created

--- client/src/pages/students/CoursesList.jsx
+++ client/src/pages/students/CoursesList.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { useNavigate, useParams } from 'react-router-dom';
+import SearchBar from '../../components/students/SearchBar';
+import CourseCard from '../../components/students/CourseCard';
 
 const CoursesList = () => {
+
+    const { navigate, allCourses } = useContext(AppContext)
+    const {input} = useParams()
+
     return (
-        <div>
-            Course List
-        </div>
+        <>
+            <div className='relative md:px-36 px-8 pt-20 text-left'>
+                <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+                    <div >
+                        <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+                        <p className='text-gray-500'><span className='text-blue-600 cursor-pointer' onClick={() => navigate('/')}>Home</span> / <span>Course List</span></p>
+                    </div>
+                    <SearchBar data={input}/>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+                    {
+                        allCourses.map((course, index)=><CourseCard course={course}/>)
+                    }
+                </div>
+            </div>
+        </>
     );
 };
 
