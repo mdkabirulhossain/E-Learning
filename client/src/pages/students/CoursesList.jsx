fbb7b0f0e0e58ca761c7e8fbc970503c86b8c374
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-18 20:27:54 +0600

    In course List added search option

--- client/src/pages/students/CoursesList.jsx
+++ client/src/pages/students/CoursesList.jsx
@@ -1,14 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 import { useNavigate, useParams } from 'react-router-dom';
 import SearchBar from '../../components/students/SearchBar';
 import CourseCard from '../../components/students/CourseCard';
+import { assets } from '../../assets/assets';
 
 const CoursesList = () => {
 
     const { navigate, allCourses } = useContext(AppContext)
     const {input} = useParams()
+    const [filteredCourse, setFilteredCourse] = useState([])
 
+    useEffect(()=>{
+        if(allCourses && allCourses.length > 0){
+            const tempCourses = allCourses.slice()
+
+            input  ? 
+            setFilteredCourse(
+                tempCourses.filter(item=>item.courseTitle.toLowerCase().includes(input.toLowerCase()))
+            )
+            :
+            setFilteredCourse(tempCourses)
+        }
+    }, [allCourses, input])
     return (
         <>
             <div className='relative md:px-36 px-8 pt-20 text-left'>
@@ -19,9 +33,17 @@ const CoursesList = () => {
                     </div>
                     <SearchBar data={input}/>
                 </div>
+
+                {
+                    input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
+                        <p>{input}</p>
+                        <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={()=> navigate('/course-list')}/>
+                    </div>
+                }
+
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
                     {
-                        allCourses.map((course, index)=><CourseCard course={course}/>)
+                        filteredCourse.map((course, index)=><CourseCard key={index} course={course}/>)
                     }
                 </div>
             </div>
