1ad826ea3c4a3fcb1a4576ecfe6dd71e509b389e
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-25 01:58:25 +0600

    Updated educator footer for mobile

--- client/src/components/educators/Footer.jsx
+++ client/src/components/educators/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
     return (
         <div>
             <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
-                <div className='flex items-center gap-4'>
+                <div className='flex items-center flex-col md:flex-row gap-0 md:gap-4 mt-4'>
                     <img onClick={()=> navigate('/')} src={assets.logo1} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
-                    <div className='hidden md:block h-7 w-px bg-gray-500/60'>
+                    {/* <div className='hidden md:block h-7 w-px bg-gray-500/60'>
 
-                    </div>
+                    </div> */}
                     <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
                         Copyright 2025 @ Kabirul. All Right Reserved.
                     </p>
