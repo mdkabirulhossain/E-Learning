e550fdacc6a50fd506541b521ea33430b8c94af8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-24 12:43:01 +0600

    Left column is created

--- client/src/pages/students/Player.jsx
+++ client/src/pages/students/Player.jsx
@@ -1,12 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 import { useParams } from 'react-router-dom';
+import { assets } from '../../assets/assets';
+import humanizeDuration from 'humanize-duration'
 
 const Player = () => {
 
     const {enrolledCourses, calculateChapterTime,} = useContext(AppContext)
     const {courseId} = useParams();
     const [courseData, setCourseData] = useState(null);
+    const [openSections, setOpenSections] = useState({})
+    const [playerData, setPlayerData] = useState(null)
 
     const getCourseData = () =>{
         enrolledCourses.map((course)=>{
@@ -24,10 +28,10 @@ const Player = () => {
             }
         ))
     }
-    
+
     useEffect(()=>{
         getCourseData()
-    }, [])
+    }, [enrolledCourses])
 
     return (
         <>
@@ -49,13 +53,13 @@ const Player = () => {
                                     <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                                         {chapter.chapterContent.map((lecture, i) => (
                                             <li key={i} className='flex items-start gap-2 py-1'>
-                                                <img src={assets.play_icon} alt="play icon" className='w-4 h-4 mt-1' />
+                                                <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="play icon" className='w-4 h-4 mt-1' />
                                                 <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default mt-1'>
                                                     <p>{lecture.lectureTitle}</p>
                                                     <div className='flex gap-2'>
-                                                        {lecture.isPreviewFree && <p onClick={() => setPlayerData({
-                                                            videoId: lecture.lectureUrl.split('/').pop()
-                                                        })} className='text-blue-500 cursor-pointer'>Preview</p>}
+                                                        {lecture.lectureUrl && <p onClick={() => setPlayerData({
+                                                            ...lecture, chapter: index + 1, lecture: i+1
+                                                        })} className='text-blue-500 cursor-pointer'>Watch</p>}
                                                         <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
                                                     </div>
                                                 </div>
