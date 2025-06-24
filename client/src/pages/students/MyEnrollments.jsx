4308102b1fc7094220e34c845c5ae72cb2e94461
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-24 11:37:28 +0600

    Updated MyEnrollment button

--- client/src/pages/students/MyEnrollments.jsx
+++ client/src/pages/students/MyEnrollments.jsx
@@ -71,7 +71,7 @@ const MyEnrollments = () => {
 
                                 </td>
                                 <td className='px-4 py-3 max-sm:hidden'>
-                                    <button onClick={()=>navigate('/player/' +course._id)} className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
+                                    <button onClick={()=>navigate('/player/' +course._id)} className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white cursor-pointer'>
                                         {progressArray[index] && progressArray[index].lectureCompleted / progressArray[index].totalLectures === 1 ? 'Completed': 'On Going'}
                                         </button>
                                 </td>
