864d8590fe1baf6acb001a239b08ef312d999960
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-25 00:02:35 +0600

    Sidebar Component is created

--- client/src/pages/educators/Educator.jsx
+++ client/src/pages/educators/Educator.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom'
+import Navbar from '../../components/educators/Navbar';
+import Sidebar from '../../components/educators/Sidebar';
 const Educator = () => {
     return (
-        <div>
-            <h1>Educator Page</h1>
-            <div>
-                { <Outlet></Outlet>}
+        <div className='text-default min-h-screen bg-white'>
+            <Navbar></Navbar>
+            <div className='flex'>
+                <Sidebar />
+                <div className='flex-1'>
+                    { <Outlet></Outlet>}
+                </div>
             </div>
         </div>
     );
