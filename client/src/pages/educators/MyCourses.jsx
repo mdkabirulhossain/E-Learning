006230b01292b9b254d0a75d400487a2d58ec87c
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-25 23:42:11 +0600

    Updated educator myCourses component

--- client/src/pages/educators/MyCourses.jsx
+++ client/src/pages/educators/MyCourses.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AppContext } from '../../context/AppContext';
+import Loading from '../../components/students/Loading';
 
 const MyCourses = () => {
-    return (
+
+    const {currency, allCourses} = useState(AppContext)
+
+    const [courses, setCourses] = useState(null)
+
+    const fechEducatorCourses = async () =>{
+        setCourses(allCourses)
+    }
+
+    useEffect(()=>{
+        fechEducatorCourses()
+    }, [])
+    return courses ?(
         <div>
            My Courses 
         </div>
-    );
+    ): <Loading />
 };
 
 export default MyCourses;
