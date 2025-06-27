3ac5e81abb0687a4507e981b5561b7bb614afbf2
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-27 18:34:22 +0600

    Initial server is created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 
+//Initialize
+const app = express()
+
+//Port
+const port = process.env.PORT || 3000
+
+//Midleware
+app.use(cors())
+
+//Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
